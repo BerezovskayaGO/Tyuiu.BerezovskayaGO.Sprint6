f844e58cca1a0aad7d7c96aac69657623963afb0
Tyuiu.BerezovskayaGO@outlook.com <Tyuiu.BerezovskayaGO@outlook.com>
2024-12-10 19:28:25 +0500

    Task6

---
+++ Tyuiu.BerezovskayaGO.Sprint6.Task6.V25/FormAbout.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Tyuiu.BerezovskayaGO.Sprint6.Task6.V25.Lib;
+namespace Tyuiu.BerezovskayaGO.Sprint6.Task6.V25
+{
+    public partial class FormAbout : System.Windows.Forms.Form
+    {
+        public FormAbout()
+        {
+            InitializeComponent();
+        }
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
